8bbf3458b2fc9e86cf8bb8fcaa0a3c2cb5e2ba54
Justine Jae <bongalos.justinejaeg8@gmail.com>
2026-04-06 21:37:17 +0800

    added 2 classes

--- SHOPPEE_TYPESHI/Program.cs
+++ SHOPPEE_TYPESHI/Program.cs
@@ -3,14 +3,14 @@ using System.ComponentModel;
 
 namespace Shopping
 {
-   class Program
+    class Program
     {
         static void Main()
         {
             while (true)
             {
                 int num = 1;
-                List<string> options = ["START SHOPPING", "VIEW PRODUCTS", "SEARCH PRODUCTS", "EXIT"];
+                List<string> options = ["START SHOPPING", "VIEW PRODUCTS", "SEARCH PRODUCTS","ADD PRODUCT", "EXIT"];
                 Console.WriteLine("WELCOME to SHOPPEE TYPESHI");
                 foreach (var words in options)
                 {
@@ -37,5 +37,30 @@ namespace Shopping
 
         }
 
-    } 
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(string name, double price, int stock)
+        {
+            Name = name;
+            Price = price;
+            Stock = stock;
+        }
+    }
+    public class Shop
+    {
+        public List<Product> products = new List<Product>
+        {
+            new Product("Laptop", 45999.99, 10),
+            new Product("Mechanical Keyboard", 3499.50, 25),
+            new Product("Wireless Mouse", 1299.99, 40),
+            new Product("27-inch Monitor", 8999.00, 15),
+            new Product("USB-C Hub", 799.75, 30)
+        };
+
+    }
 }
