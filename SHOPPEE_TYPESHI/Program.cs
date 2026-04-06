b06327e24cbd71215afcea17f372f733e973608e
Justine Jae <bongalos.justinejaeg8@gmail.com>
2026-04-06 21:19:35 +0800

    built the menu

--- SHOPPEE_TYPESHI/Program.cs
+++ SHOPPEE_TYPESHI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Shopping
 {
@@ -6,15 +7,35 @@ namespace Shopping
     {
         static void Main()
         {
-            int num = 1;
-            List<string> options = ["START SHOPPING","VIEW PRODUCTS", "SEARCH PRODUCTS", "EXIT"];
-            Console.WriteLine("WELCOME to SHOPPEE TYPESHI");
-            foreach (var words in options)
+            while (true)
             {
-                Console.WriteLine($"{num}.  {words}");
-                num++;
+                int num = 1;
+                List<string> options = ["START SHOPPING", "VIEW PRODUCTS", "SEARCH PRODUCTS", "EXIT"];
+                Console.WriteLine("WELCOME to SHOPPEE TYPESHI");
+                foreach (var words in options)
+                {
+                    Console.WriteLine($"{num}.  {words}");
+                    num++;
+                }
+                string customer_descision = Console.ReadLine();
+                switch (customer_descision)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+
             }
 
         }
+
     } 
 }
