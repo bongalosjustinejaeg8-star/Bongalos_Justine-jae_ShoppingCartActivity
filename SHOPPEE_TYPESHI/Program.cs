86a05661d8fccd260724ddb10726b695c77b34ca
Justine Jae <bongalos.justinejaeg8@gmail.com>
2026-04-06 21:13:09 +0800

    first commit

---
+++ SHOPPEE_TYPESHI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shopping
+{
+   class Program
+    {
+        static void Main()
+        {
+            int num = 1;
+            List<string> options = ["START SHOPPING","VIEW PRODUCTS", "SEARCH PRODUCTS", "EXIT"];
+            Console.WriteLine("WELCOME to SHOPPEE TYPESHI");
+            foreach (var words in options)
+            {
+                Console.WriteLine($"{num}.  {words}");
+                num++;
+            }
+
+        }
+    } 
+}
